877015d9098a4c2bed00bb476d3f42f5d883ef4c
gsuiz <156917760+gsuiz@users.noreply.github.com>
2025-01-30 10:41:05 -0300

    configurada as rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,8 @@ import Home from "./routes/Home"
 import NewProject from "./routes/NewProject"
 import Projects from "./routes/Projects"
 import EditProject from "./routes/EditProject"
+import Empresa from "./routes/Empresa"
+import Contato from "./routes/Contato"
 
 export default () => createBrowserRouter([
     {
@@ -24,8 +26,16 @@ export default () => createBrowserRouter([
                 element:<Projects/>
             },
             {
-                path:"/projetos/:index",
+                path:"/projetos/:id",
                 element:<EditProject/>
+            },
+            {
+                path:"/empresa",
+                element:<Empresa/>
+            },            
+            {
+                path:"/contato",
+                element:<Contato/>
             }
         ]
     }
